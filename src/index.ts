4876f7b9532b83ba77d3190df0dc893553e4add9
son0925 <shs00925@naver.com>
2024-05-14 17:26:14 +0900

    restart

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,17 @@
 import express from 'express';
 import morgan from 'morgan';
-import { AppDataSource } from './data-source';
 
 const app = express();
 
 app.use(express.json());
-app.use(morgan('dev'));
-
+app.use(morgan('short'));
 
 app.get('/', (req,res) => {
-  res.send('Hello TypeScript');
+  res.send('Hello TypeScript')
 })
-
-AppDataSource.initialize()
-  .then(() => {
-    console.log('성공');
-  })
-  .catch((err) => {
-    console.error(err);
-  })
 
 
 const port = 4000;
 app.listen(port, () => {
-  console.log(`Listinning on ${port}`)
+  console.log(`Server Running at http://localhost${port}`)
 })
